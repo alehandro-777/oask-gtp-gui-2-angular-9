5bd52b6f1f18646f6c7b25f8d3ae77e98b3e4c34
rekalo.alex <rekalo.alex@hotmail.com>
2021-01-01 22:05:13 +0200

    dev

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express')
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('./dist/oask-gtp-data-prep-gui'));
+app.use(express.static('./dist/e-commer-angular9'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/oask-gtp-data-prep-gui/'});
+    res.sendFile('index.html', {root: 'dist/e-commer-angular9/'});
 });
 
 console.log(`Server started at port: ${port}`)
